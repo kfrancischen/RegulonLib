351f3e686ce368449d7c432b44fecf18620acc17
Francis Chen <kfrancischen@gmail.com>
2012-08-20 13:28:12 +0800

    adding class LinkLabel

--- USTC_Clotho_App/main_GUI.java
+++ USTC_Clotho_App/main_GUI.java
@@ -9,55 +9,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import javax.swing.*;
-
-
-
 import java.util.Vector;
 
-class LinkLabel extends JLabel {  
-    private String text, url;  
-    private boolean isSupported;  
-  
-    public LinkLabel(String text, String url) {  
-        this.text = text;  
-        this.url = url;  
-        try {  
-            this.isSupported = Desktop.isDesktopSupported()  
-            && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE);  
-        } catch (Exception e) {  
-            this.isSupported = false;  
-        }  
-        setText(false);  
-        addMouseListener(new MouseAdapter() {  
-            public void mouseEntered(MouseEvent e) {  
-                setText(isSupported);  
-                if (isSupported)  
-                    setCursor(new Cursor(Cursor.HAND_CURSOR));  
-            }  
-  
-            public void mouseExited(MouseEvent e) {  
-                setText(false);  
-            }  
-  
-            public void mouseClicked(MouseEvent e) {  
-                try {  
-                    Desktop.getDesktop().browse(  
-                            new java.net.URI(LinkLabel.this.url));  
-                } catch (Exception ex) {  
-                }  
-            }  
-        });  
-    }  
-  
-    private void setText(boolean b) {  
-        if (!b)  
-            setText("<html><font color=blue><u>" + text);  
-        else  
-            setText("<html><font color=red><u>" + text);  
-    }  
-  
-       
-}  
 
 public class main_GUI{
 
